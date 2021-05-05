6b3a73a66a601318bb310fe6b07ae8bcf8033628
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-05 15:05:23 +0600

    Pass State as Props to Child Components

--- state_toggle.js
+++ state_toggle.js
@@ -157,3 +157,36 @@ class MyForm extends React.Component {
   }
 }
 
+//Pass State as Props to Child Components
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         {/* Change code below this line */}
+         <Navbar name={this.state.name}/>
+         {/* Change code above this line */}
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      {/* Change code below this line */}
+      <h1>Hello, my name is:{this.props.name} </h1>
+      {/* Change code above this line */}
+    </div>
+    );
+  }
+};
