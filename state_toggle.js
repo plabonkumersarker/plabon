8b5e07b488303bf0c550732f30a46a6736aa51be
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-06 23:13:53 +0600

    Use the Lifecycle Method componentWillMount

--- state_toggle.js
+++ state_toggle.js
@@ -248,3 +248,19 @@ class RenderInput extends React.Component {
     );
   }
 };
+
+//Use the Lifecycle Method componentWillMount
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+console.log("Hello");
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
