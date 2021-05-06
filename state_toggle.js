0f408e9a9791593d6e09d5535a57604522885639
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-06 23:50:55 +0600

    Add Inline Styles in React

--- state_toggle.js
+++ state_toggle.js
@@ -384,3 +384,21 @@ class Colorful extends React.Component {
     );
   }
 };
+
+//Add Inline Styles in React
+
+const styles={
+  color: "purple",
+  fontSize: 40,
+  border: "2px solid purple"
+}
+// Change code above this line
+class Colorful extends React.Component {
+  render() {
+    // Change code below this line
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+    // Change code above this line
+  }
+};
