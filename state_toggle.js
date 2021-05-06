8d6aa404fbf8001f7645495a7cb0833e346857b8
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-06 23:18:37 +0600

    Use the Lifecycle Method componentDidMount

--- state_toggle.js
+++ state_toggle.js
@@ -264,3 +264,30 @@ console.log("Hello");
     return <div />
   }
 };
+  
+  //Use the Lifecycle Method componentDidMount
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>Active Users: {this.state.activeUsers}</h1>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
