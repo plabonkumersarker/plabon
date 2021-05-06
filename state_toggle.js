1af4d8e33b5e7e43f663877f70ade39658236284
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-06 23:42:24 +0600

    Introducing Inline Styles

--- state_toggle.js
+++ state_toggle.js
@@ -374,3 +374,13 @@ class Controller extends React.Component {
     );
   }
 }
+
+//Introducing Inline Styles
+
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color: "red", fontSize: "72px"}}>Big Red</div>
+    );
+  }
+};
