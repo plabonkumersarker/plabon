9f1ccebc412bda17d409374bee90cb6c2ab2d777
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-07 22:54:59 +0600

    Change Inline CSS Conditionally Based on Component State

--- state_toggle.js
+++ state_toggle.js
@@ -637,3 +637,39 @@ class GameOfChance extends React.Component {
     );
   }
 }
+
+//Change Inline CSS Conditionally Based on Component State
+
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // Change code below this line
+let newStyle ={
+  border: "3px solid red"
+}
+{this.state.input.length>15 ? inputStyle=newStyle:inputStyle=inputStyle}
+    // Change code above this line
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
