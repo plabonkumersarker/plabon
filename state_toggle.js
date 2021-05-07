37bf4da531a2679dab4bd815217dbce6a7c080cd
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-07 23:35:33 +0600

    Render React on the Server with renderToString

--- state_toggle.js
+++ state_toggle.js
@@ -793,3 +793,17 @@ class MyComponent extends React.Component {
     );
   }
 }
+
+//Render React on the Server with renderToString
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
+};
+
+// Change code below this line
+ReactDOMServer.renderToString(<App/>)
