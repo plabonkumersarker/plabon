4d1ff026f16b962a03f1c0ae729356767ac070eb
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-07 23:32:57 +0600

    Use Array.filter() to Dynamically Filter an Array

--- state_toggle.js
+++ state_toggle.js
@@ -746,3 +746,50 @@ function Frameworks() {
     </div>
   );
 };
+
+//Use Array.filter() to Dynamically Filter an Array
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    };
+  }
+  render() {
+
+    const usersOnline = this.state.users.filter(user => user.online == true); // Change this line
+    const renderOnline = usersOnline.map(x=><li key={x.username}>{x.username}</li>); // Change this line
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
+}
