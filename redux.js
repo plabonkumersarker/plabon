ecaa19cd5ebbe551764973753a53ab78989b0255
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-10 23:26:21 +0600

    Define an Action Creator

--- redux.js
+++ redux.js
@@ -26,4 +26,14 @@ const action = {
   type: 'LOGIN'
 }
 
+//Define an Action Creator
+
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+
+function actionCreator(){
+  return action;
+}
   
