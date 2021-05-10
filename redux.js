d4f392e1fd59cec42ab7fb4fc2f7b2aa41f3f7d5
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-10 23:58:57 +0600

    Use a Switch Statement to Handle Multiple Actions

--- redux.js
+++ redux.js
@@ -77,4 +77,37 @@ const loginAction = () => {
   }
 };
 
+//Use a Switch Statement to Handle Multiple Actions
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  // Change code below this line
+switch(action.type){
+  case "LOGIN":
+    return {authenticated: true};
+  case "LOGOUT":
+    return {authenticated: false};
+  default:
+    return state;
+}
+  // Change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
+
   
