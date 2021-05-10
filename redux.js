f247f4790f0b9a2f1c1bb57daf3286d41d68dd70
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-10 23:44:18 +0600

    Handle an Action in the Store

--- redux.js
+++ redux.js
@@ -53,4 +53,28 @@ const loginAction = () => {
 
 store.dispatch(loginAction());
 
+//Handle an Action in the Store
+
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+if(action.type === "LOGIN"){
+  return {login: true};
+}else{
+  return state;
+}
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
   
