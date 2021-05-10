0e8a99028aef5a82457fe3cc51dbcab94511033b
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-10 23:29:33 +0600

    Dispatch an Action Event

--- redux.js
+++ redux.js
@@ -36,4 +36,21 @@ const action = {
 function actionCreator(){
   return action;
 }
+
+//Dispatch an Action Event
+
+const store = Redux.createStore(
+  (state = {login: false}) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Dispatch the action here:
+
+store.dispatch(loginAction());
+
   
