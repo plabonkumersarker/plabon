1f029cf40196ecb3b244b63439d9e44035ba19db
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-11 00:06:35 +0600

    Register a Store Listener

--- redux.js
+++ redux.js
@@ -152,4 +152,31 @@ const logoutUser = () => {
   }
 };
 
-  
+//Register a Store Listener
+
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+// Change code below this line
+store.subscribe(()=>count += 1)
+// Change code above this line
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
