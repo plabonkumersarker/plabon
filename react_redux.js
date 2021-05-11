70aa5aba6fc577b1bed6b4e9e3a427c67dc9f46e
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-11 23:32:42 +0600

    Manage State Locally First

--- react_redux.js
+++ react_redux.js
@@ -15,3 +15,48 @@ class DisplayMessages extends React.Component {
       return <div />
     }
   };
+    
+//Manage State Locally First
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+  }
+  // Add handleChange() and submitMessage() methods here
+  handleChange(data){
+    this.setState({
+      input: data.target.value,
+      messages: this.state.messages
+    })
+  }
+
+  submitMessage(){
+    this.setState({
+      input: "",
+      messages: [...this.state.messages, this.state.input]
+    })
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        { /* Render an input, button, and ul below this line */ }
+        <input onChange={this.handleChange.bind(this)} value={this.state.input}/>
+        <button onClick={this.submitMessage.bind(this)}>
+          Submit
+        </button>
+        <ul>
+          {this.state.messages.map((x)=> {
+            return <li>{x}</li>
+          })}
+        </ul>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
