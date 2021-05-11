6e0645fbe494e813ac1e42b773c1d96113296383
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-11 16:24:20 +0600

    Send Action Data to the Store

--- redux.js
+++ redux.js
@@ -222,3 +222,34 @@ const rootReducer = Redux.combineReducers({
 
 const store = Redux.createStore(rootReducer);
 
+//Send Action Data to the Store
+
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+    // Change code below this line
+    case ADD_NOTE:
+      return action.text;
+    // Change code above this line
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+  // Change code below this line
+return {
+  type : ADD_NOTE,
+  text :note 
+}
+  // Change code above this line
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
+
+
