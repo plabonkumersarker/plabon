fafc78738c55071ee00e39af6f27ee556183d41b
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-11 17:36:50 +0600

    Use the Spread Operator on Arrays

--- redux.js
+++ redux.js
@@ -359,3 +359,25 @@ const addToDo = (todo) => {
 }
 
 const store = Redux.createStore(immutableReducer);
+
+//Use the Spread Operator on Arrays
+
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+  switch(action.type) {
+    case 'ADD_TO_DO':
+      // Don't mutate state here or the tests will fail
+      let newTodo = [...state,action.todo]
+      return newTodo
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: 'ADD_TO_DO',
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
