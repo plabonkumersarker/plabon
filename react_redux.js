8d155732292f2e1589b5735a77511cd9696ebe8e
Plabon kumer Sarker <60446796+plabonkumersarker@users.noreply.github.com>
2021-05-12 22:32:54 +0600

    Map Dispatch to Props

--- react_redux.js
+++ react_redux.js
@@ -185,5 +185,24 @@ function mapStateToProps(state){
   }
 }
 
+//Map Dispatch to Props
+
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// Change code below this line
+
+function mapDispatchToProps(dispatch){
+  return({
+    submitNewMessage : function(message){
+      dispatch(addMessage(message))
+    }
+  })
+}
+
 
 
